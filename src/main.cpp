ec03ffd2ca952a851fa339e4b67999751c4bbfec
Gabriel Ruiz <garuizbido@gmail.com>
2023-02-16 17:13:53 -0500

    Changed board to Elegoo Nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 
+/*
+ *   Basic example code for controlling a stepper without library
+ *
+ *   by Dejan, https://howtomechatronics.com
+ */
+
 // defines pins
-#define stepPin 2
-#define dirPin 3
-#define stepPin2 4
-#define dirPin2 5
+#define stepPin 5
+#define dirPin 4
+#define stepPin2 3
+#define dirPin2 2
 
 void setup()
 {
   // Sets the two pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
 }
 void loop()
 {
@@ -41,7 +49,7 @@ void loop()
   for (int x = 0; x < 200; x++)
   {
     digitalWrite(stepPin2, HIGH);
-    delayMicroseconds(700); // by changing this time delay between the steps we can change the rotation speed
+    delayMicroseconds(700); // by changing this time delay between the steps we can change the rotation speedv
     digitalWrite(stepPin2, LOW);
     delayMicroseconds(700);
   }
