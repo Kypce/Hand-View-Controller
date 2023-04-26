ca03ffd8e4259d8fbb0ce0aa391a7d59e51f5a55
Gabriel Ruiz <garuizbido@gmail.com>
2023-04-26 17:52:02 -0400

    Added steps per mm

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,72 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include <AccelStepper.h>
+#include <ezButton.h>
 
 // Define "home_switches" for resetting the spinner and pins
-#define x_home_switch 7
-#define y_home_switch 8
 
 // *** GLOBAL VARIABLE DECLARATIONS
 // Setup the pin grid size
-const int x = 3;
-const int y = 3;
-const int quadrantNumbers = x * y;
+const int x = 4;
+const int y = 4;
+const int quadrantTotal = x * y;
+
+long stepsMoveX = 16.98;
+long stepsMoveY = 273.97;
 
 // Size 16 array for the states of the pins (0 = off, 1 = half, 2 = full)
-int pinStates[quadrantNumbers];
-
-// Setup the current x/y position
-int currX;
-int currY;
-// *** END OF GLOBAL DECLARATIONS
+int pinStates[quadrantTotal];
 
 // Setup bluetooth connection and stepper motors 1 and 2
 SoftwareSerial Bluetooth(0, 1); // RX, TX
-AccelStepper stepper1(1, 2, 3);
-AccelStepper stepper2(1, 4, 5);
+AccelStepper stepperTop(1, 2, 3);
+AccelStepper stepperBottom(1, 4, 5);
+ezButton limitSwitch(6);
 
 // Will reset the spinner to the "first position"
 void resetSpinner()
 {
-  stepper1.setSpeed(100);
-  stepper2.setSpeed(100);
-  pinMode(x_home_switch, INPUT_PULLUP);
-  pinMode(y_home_switch, INPUT_PULLUP);
+  stepperTop.setSpeed(500);
+  stepperBottom.setSpeed(500);
 
   Serial.println("Stepper motors are homing to start position . . .");
-  while (digitalRead(x_home_switch))
+  while (true)
   {
-    // TODO
+    limitSwitch.loop();
+    if (limitSwitch.isPressed())
+    {
+      Serial.println("The limit switch: UNTOUCHED -> TOUCHED");
+      break;
+    }
+    stepperTop.runSpeed();
+    stepperBottom.runSpeed();
   }
-
-  while (digitalRead(y_home_switch))
-  {
-    // TODO
-  }
+  stepperTop.setCurrentPosition(0);
+  stepperBottom.setCurrentPosition(0);
 }
 
 // Will reset all pins to their off position
 void resetPins()
 {
-  stepper1.setSpeed(100);
-  stepper2.setSpeed(100);
+  stepperTop.setSpeed(100);
+  stepperBottom.setSpeed(100);
 
   // Sequence to reset all pins
   // TODO
 
   // Reset back to start position
-  resetSpinner();
 }
 
 // Reset x/y position variables and pinStates' values`
 void resetPositionValues()
 {
-  currX = 0;
-  currY = 0;
-  for (int i = 0; i < sizeof(pinStates); i++)
+  for (int i = 0; i < quadrantTotal; i++)
     pinStates[i] = 0;
 }
 
 // Will move to state change pins and initiate them
 void updatePinStates(int *stateChanges)
 {
-  // TODO
-
   Serial.println("Updating pins to reflect the current environment . . .");
 }
 
@@ -99,26 +94,23 @@ void setup()
   Bluetooth.begin(9600);
   Serial.begin(9600);
   Serial.println("Waiting for command...");
-  stepper1.setMaxSpeed(1000);
-  stepper2.setMaxSpeed(1000);
-  stepper1.setSpeed(0);
-  stepper2.setSpeed(0);
+  stepperTop.setMaxSpeed(1000);
+  stepperBottom.setMaxSpeed(1000);
+  stepperTop.setAcceleration(6000);
+  stepperBottom.setAcceleration(6000);
+  stepperTop.setSpeed(0);
+  stepperBottom.setSpeed(0);
 
   // Call reset() functions to reset position and corresponding values
-  // resetSpinner();
+  resetSpinner();
   // resetPins();
   // resetPositionValues();
 }
 
 void loop()
 {
-  if (Bluetooth.available())
-  { // wait for data received
-    int data = Bluetooth.read();
-    Serial.println((data / 2) * 100);
-    stepper1.setSpeed((data / 2) * 100);
-    stepper2.setSpeed((data / 2) * 100);
-  }
-  stepper1.runSpeed();
-  stepper2.runSpeed();
+  stepperTop.moveTo(-1369.85);
+  stepperBottom.moveTo(1369.85);
+  stepperTop.run();
+  stepperBottom.run();
 }
